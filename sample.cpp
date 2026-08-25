c466ec2004158b4670ca34a63a49395712f3af66
4D-24\koyo <a>
2026-08-25 13:52:35 +0900

    template

--- sample.cpp
+++ sample.cpp
@@ -1,29 +1,19 @@
 ﻿#include <iostream>
 
-int max_int(int a,int b)
+template<class T>
+T max(T a, T b)
 {
     return a > b ? a : b;
 }
-
-long max_long(long a,long b)
-{
-    return a > b ? a : b;
-}
-
-float max_float(float a,float b)
-{
-    return a > b ? a : b;
-}
-
 
 int main()
 {
-    float a = 10.1;
-    float b = 10.2;
+    int a = 10;
+    int b = 10.1;
+	float c = 10.1;
+	float d = 10.2;
 
-	std::cout << "max: " << max_int(a, b) << std::endl;
-	std::cout << "max: " << max_long(a, b) << std::endl;
-    std::cout << "max: " << max_float(a, b) << std::endl;
-
+	std::cout << "max: " << max(a, b) << std::endl;
+    std::cout << "max: " << max(c, d) << std::endl;
 }
 
