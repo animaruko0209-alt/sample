a0d86779ea015deb8a6782e87993086141cd2c90
4D-24\koyo <a>
2026-08-25 14:28:19 +0900

    template 2

--- sample.cpp
+++ sample.cpp
@@ -1,19 +1,34 @@
 ﻿#include <iostream>
 
-template<class T>
-T max(T a, T b)
-{
-    return a > b ? a : b;
+template<typename T,std::size_t N>
+
+class StaticArray {
+	T data[N];
+public:
+	constexpr T& operator[](std::size_t i) { return data[i]; }
+	constexpr const T& operator[](std::size_t i) const { return data[i]; }
+	constexpr std::size_t size() const { return N; }
+};
+
+template<typename T>struct IsPointer {
+	static constexpr bool value = false;
+};
+
+template<typename U>struct IsPointer<U*> {
+	static constexpr bool value = true;
+};
+
+
+
+int main() {
+	StaticArray<int, 30>data;
+	IsPointer<int>a;
+
+	std::cout << a.value << std::endl;
+
+	for(auto i = 0; i < data.size(); ++i) {
+		data[i] = i;
+	}
+
+	std::cout << data[5] << std::endl;
 }
-
-int main()
-{
-    int a = 10;
-    int b = 10.1;
-	float c = 10.1;
-	float d = 10.2;
-
-	std::cout << "max: " << max(a, b) << std::endl;
-    std::cout << "max: " << max(c, d) << std::endl;
-}
-
